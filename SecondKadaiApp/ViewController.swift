f7cd687c2186459675159218d321e1454a51f6ba
小本裕也 <komotoyuuya@komotoyuuya-no-imac.local>
2017-02-12 17:22:14 +0900

    あいさつアプリ

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TextField: UITextField!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func prepare(for segue:UIStoryboardSegue, sender:Any?){
+        let viewcontroller2:ViewController2 = segue.destination as! ViewController2
+        viewcontroller2.mozi = TextField.text!
+    }
+    
+    @IBAction func modoru(segue:UIStoryboardSegue){
+    }
+    
 }
 
---
+++ SecondKadaiApp/ViewController2.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController2.swift
+//  SecondKadaiApp
+//
+//  Created by 小本裕也 on 2017/02/12.
+//  Copyright © 2017年 小本裕也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    @IBOutlet weak var Label: UILabel!
+    
+    var mozi:String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        Label.text = ("こんにちは、\(mozi)さん")
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
